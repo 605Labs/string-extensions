12b59b062060b5638c8e6c12e782f91fe62208f1
Brian Behm <brian.behm@gmail.com>
2022-02-08 04:30:25 +0000

    Add initial project structure

---
+++ 605Labs.Extensions.String.Tests/StringExtensionsTests.cs
@@ -0,0 +1,182 @@
+using FluentAssertions;
+using Xunit;
+
+namespace _605Labs.Extensions.String.Tests;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void StringEmpty_EqualsEmptyDoubleQuotes()
+    {
+        // Arrange
+        var testEmptyString = string.Empty;
+        var testDoubleQuoteEmptyString = "";
+
+        // Act, Assert
+        Assert.Equal(testEmptyString, testDoubleQuoteEmptyString);
+    }
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData("", null)]
+    [InlineData(" ", null)]
+    [InlineData("   ", null)]
+    [InlineData("some value", "some value")]
+    public void GetNullIfEmpty_GivenValue_ReturnsExpectedResult(string testString, string expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.GetNullIfEmptyString();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+
+    [Theory]
+    [InlineData(null, "")]
+    [InlineData(" ", " ")]
+    [InlineData("non-empty string", "non-empty string")]
+    public void GetEmptyStringIfNull_GivenValue_ReturnsExpectedResult(string testString, string expectedResult)
+    {
+        // Arrange
+        
+        // Act
+        var result = testString.GetEmptyStringIfNull();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("", "someDefaultValue", "someDefaultValue")]
+    [InlineData(null, "someDefaultValue", "someDefaultValue")]
+    [InlineData(" ", "someDefaultValue", " ")]
+    [InlineData("testString", "someDefaultValue", "testString")]
+    public void GetDefaultIfEmpty_GivenValue_ReturnsExpectedResult(string testString, string defaultValue, string expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.GetDefaultIfEmpty(defaultValue);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("", true)]
+    [InlineData(" ", false)]
+    [InlineData(null, false)]
+    [InlineData("testing123", false)]
+    public void IsEmpty_GivenValue_ReturnsExpectedResult(string testString, bool expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.IsEmpty();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("123", true)]
+    [InlineData("-7890", true)]
+    [InlineData("2147483647", true)]
+    [InlineData("-2147483648", true)]
+    [InlineData("92147483647", false)]
+    [InlineData("-92147483648", false)]
+    [InlineData("", false)]
+    [InlineData(" ", false)]
+    [InlineData(null, false)]
+    [InlineData("testing123", false)]
+    [InlineData("789testing", false)]
+    public void IsInteger_GivenValue_ReturnsExpectedResult(string testString, bool expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.IsInteger();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("123456.87", true)]
+    [InlineData("123,456.87", true)]
+    [InlineData("123,456.87654", true)]
+    [InlineData("123", true)]
+    [InlineData("-7890", true)]
+    [InlineData("2147483647", true)]
+    [InlineData("-2147483648", true)]
+    [InlineData("92147483647", true)]
+    [InlineData("-92147483648", true)]
+    [InlineData("", false)]
+    [InlineData(" ", false)]
+    [InlineData(null, false)]
+    [InlineData("testing123", false)]
+    [InlineData("789testing", false)]
+    public void IsNumeric_GivenValue_ReturnsExpectedResult(string testString, bool expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.IsNumeric();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("testing123", "321gnitset")]
+    [InlineData("", "")]
+    [InlineData(" ", " ")]
+    [InlineData(null, null)]
+    public void Reverse_GivenValue_ReturnsExpectedResult(string testString, string expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.Reverse();
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("testing123", 4, "test")]
+    [InlineData("tst", 5, "tst")]
+    [InlineData("", 5, "")]
+    [InlineData(" ", 7, " ")]
+    [InlineData(null, 10, null)]
+    public void Left_GivenValue_ReturnsExpectedResult(string testString, int count, string expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.Left(count);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData("testing123", 4, "g123")]
+    [InlineData("tst", 5, "tst")]
+    [InlineData("", 5, "")]
+    [InlineData(" ", 7, " ")]
+    [InlineData(null, 10, null)]
+    public void Right_GivenValue_ReturnsExpectedResult(string testString, int count, string expectedResult)
+    {
+        // Arrange
+
+        // Act
+        var result = testString.Right(count);
+
+        // Assert
+        result.Should().Be(expectedResult);
+    }
+}
---
+++ 605Labs.Extensions.String/StringExtensions.cs
@@ -0,0 +1,97 @@
+﻿namespace _605Labs.Extensions.String;
+
+public static class StringExtensions
+{
+    public static string GetNullIfEmptyString(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            return null;
+        }
+
+        return str;
+    }
+
+    public static string GetEmptyStringIfNull(this string str)
+    {
+        if (str == null)
+        {
+            return "";
+        }
+
+        return str;
+    }
+
+    public static string GetDefaultIfEmpty(this string str, string defaultValue)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return defaultValue;
+        }
+
+        return str;
+    }
+
+    public static bool IsEmpty(this string str)
+    {
+        return str == "" || str == string.Empty;
+    }
+
+    public static bool IsInteger(this string str)
+    {
+        return int.TryParse(str, out var _);
+    }
+
+    public static bool IsNumeric(this string str)
+    {
+        return double.TryParse(str, out var _);
+    }
+
+    public static string Reverse(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str) || str.Length == 1)
+        {
+            return str;
+        }
+
+        var reversed = new char[str.Length];
+        var index = 0;
+
+        for (var i=reversed.Length; i>0; i--)
+        {
+            reversed[index++] = str[i-1];
+        }
+
+        return new string(reversed);
+    }
+
+    public static string Left(this string str, int count)
+    {
+        if (str == null)
+        {
+            return str;
+        }
+
+        if (str.Length <= count)
+        {
+            return str;
+        }
+
+        return str.Substring(0, count);
+    }
+
+    public static string Right(this string str, int count)
+    {
+        if (str == null)
+        {
+            return str;
+        }
+
+        if (str.Length <= count)
+        {
+            return str;
+        }
+
+        return str.Substring(str.Length - count);
+    }
+}
